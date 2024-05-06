7963f4e52eb15d865a078ef533b299fe18ef1463
carlos canacue <carlosduardcar@gmail.com>
2024-05-06 14:38:11 -0500

    first commit

---
+++ src/test/java/com/inn/frutimoon/FrutimoonManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inn.frutimoon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrutimoonManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
