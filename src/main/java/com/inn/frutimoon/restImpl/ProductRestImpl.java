78c458b04d68c35a78476607a203a212ac6f1dde
carlos canacue <carlosduardcar@gmail.com>
2024-05-10 16:25:17 -0500

    product completed

--- src/main/java/com/inn/frutimoon/restImpl/ProductRestImpl.java
+++ src/main/java/com/inn/frutimoon/restImpl/ProductRestImpl.java
@@ -1,4 +1,92 @@
 package com.inn.frutimoon.restImpl;
 
-public class ProductRestImpl {
+import com.inn.frutimoon.constants.FrutimoonConstants;
+import com.inn.frutimoon.rest.ProductRest;
+import com.inn.frutimoon.service.ProductService;
+import com.inn.frutimoon.utils.FrutimoonUtils;
+import com.inn.frutimoon.wrapper.ProductWrapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class ProductRestImpl implements ProductRest {
+
+    @Autowired
+    ProductService productService;
+
+    @Override
+    public ResponseEntity<String> addNewProduct(Map<String, String> requestMap) {
+        try{
+            return productService.addNewProduct(requestMap);
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+        return FrutimoonUtils.getResponseEntity(FrutimoonConstants.SOMETHING_WENT_WRONG, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @Override
+    public ResponseEntity<List<ProductWrapper>> getAllProduct() {
+        try{
+            return productService.getAllProduct();
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @Override
+    public ResponseEntity<String> updateProduct(Map<String, String> requestMap) {
+        try{
+            return productService.updateProduct(requestMap);
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+        return FrutimoonUtils.getResponseEntity(FrutimoonConstants.SOMETHING_WENT_WRONG, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @Override
+    public ResponseEntity<String> deleteProduct(Integer id) {
+        try{
+            return productService.deleteProduct(id);
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+        return FrutimoonUtils.getResponseEntity(FrutimoonConstants.SOMETHING_WENT_WRONG, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @Override
+    public ResponseEntity<String> updateStatus(Map<String, String> requestMap) {
+        try{
+            return productService.updateStatus(requestMap);
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+        return FrutimoonUtils.getResponseEntity(FrutimoonConstants.SOMETHING_WENT_WRONG, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @Override
+    public ResponseEntity<List<ProductWrapper>> getByCategory(Integer id) {
+        try{
+            return productService.getByCategory(id);
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @Override
+    public ResponseEntity<ProductWrapper> getProductById(Integer id) {
+        try{
+            return productService.getProductById(id);
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+        return  new ResponseEntity<>(new ProductWrapper(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }
