2358271f92d979bcdce6d5586bdc854ecb526369
carlos canacue <carlosduardcar@gmail.com>
2024-05-10 16:12:11 -0500

    category completed

--- src/main/java/com/inn/frutimoon/dao/CategoryDao.java
+++ src/main/java/com/inn/frutimoon/dao/CategoryDao.java
@@ -1,4 +1,11 @@
 package com.inn.frutimoon.dao;
 
-public interface CategoryDao {
+import com.inn.frutimoon.POJO.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CategoryDao extends JpaRepository<Category, Integer> {
+
+    List<Category> getAllCategory();
 }
--- src/main/java/com/inn/frutimoon/restImpl/CategoryRestImpl.java
+++ src/main/java/com/inn/frutimoon/restImpl/CategoryRestImpl.java
@@ -1,4 +1,52 @@
 package com.inn.frutimoon.restImpl;
 
-public class CategoryRestImpl {
+import com.inn.frutimoon.POJO.Category;
+import com.inn.frutimoon.constants.FrutimoonConstants;
+import com.inn.frutimoon.rest.CategoryRest;
+import com.inn.frutimoon.service.CategoryService;
+import com.inn.frutimoon.utils.FrutimoonUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class CategoryRestImpl implements CategoryRest {
+
+    @Autowired
+    CategoryService categoryService;
+
+    @Override
+    public ResponseEntity<String> addNewCategory(Map<String, String> requestMap) {
+        try{
+            return categoryService.addNewCategory(requestMap);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return FrutimoonUtils.getResponseEntity(FrutimoonConstants.SOMETHING_WENT_WRONG, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @Override
+    public ResponseEntity<List<Category>> getAllCategories(String filterValue) {
+        try{
+            return categoryService.getAllCategory(filterValue);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return new ResponseEntity(new ArrayList<>(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @Override
+    public ResponseEntity<String> updateCategory(Map<String, String> requestMap) {
+        try{
+            return categoryService.updateCategory(requestMap);
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+        return FrutimoonUtils.getResponseEntity(FrutimoonConstants.SOMETHING_WENT_WRONG, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }
