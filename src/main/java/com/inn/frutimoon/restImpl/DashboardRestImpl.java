d51ff406adb6363f0b09bf7f160829bab57d50c6
carlos canacue <carlosduardcar@gmail.com>
2024-05-14 22:23:04 -0500

    add dashboard

---
+++ src/main/java/com/inn/frutimoon/restImpl/DashboardRestImpl.java
@@ -0,0 +1,21 @@
+package com.inn.frutimoon.restImpl;
+
+import com.inn.frutimoon.rest.DashboardRest;
+import com.inn.frutimoon.service.DashboardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class DashboardRestImpl implements DashboardRest {
+
+    @Autowired
+    DashboardService dashboardService;
+
+    @Override
+    public ResponseEntity<Map<String, Object>> getCount() {
+        return dashboardService.getCount();
+    }
+}
